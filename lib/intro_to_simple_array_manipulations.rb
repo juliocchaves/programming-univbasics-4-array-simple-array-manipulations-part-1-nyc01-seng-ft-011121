22dc035650786f73883bce1b90a6c0903bf67690
juliochavesnyc <juliochavesnyc@gmail.com>
2020-11-29 03:18:42 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,22 +3,3 @@ def using_push(array, string)
   next_color = "violet"
   colors_in_the_rainbow.push(next_color)
 end
-
-def using_unshift(array, string)
-  array.unshift(string)
-end
-
-def using_pop(array)
-  array.pop 
-end
-
-def pop_with_args(array)
-  array.pop[2]
-end
-
-def using_shift(array)
-  array.shift 
-end
-
-def shift_with_args(array)
-  array.shift[2]

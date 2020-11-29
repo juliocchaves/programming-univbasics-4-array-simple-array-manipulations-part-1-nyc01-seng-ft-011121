fcf050f32740acfeb229dd1f57642246d991fc8d
juliochavesnyc <juliochavesnyc@gmail.com>
2020-11-29 03:53:52 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,7 +1,5 @@
-def using_push(countries_in_western_africa, next_country)
-  countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
-  next_country = "Niger"
-  countries_in_western_africa.push(next_country)
+def using_push(array, string)
+  array.push(string)
 end
 
 def using_unshift(array, string)
